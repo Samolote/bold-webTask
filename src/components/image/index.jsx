1d2091929941feb9f671a8a7a381f8a92f9c5d33
Wojciech Łukowski <wojtek304@gmail.com>
2024-11-07 12:59:57 +0100

    Update Image component

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,11 +1,68 @@
-const Image = ({ src, alt, className, width = 'auto', height = 'auto' }) => {
+import React, { useState } from 'react';
 
-  const handleImageError = (e) => {
-    e.target.alt = 'Image failed to load';
+const Image = ({
+  src,
+  alt,
+  wrapperClassName,
+  imageClassName,
+  width = 'auto',
+  height = 'auto',
+  objectFit = 'cover',
+  placeholderSrc = '',
+  fallbackSrc = '',
+  lazyLoad = true,
+  srcSet,
+  sizes,
+  onLoad,
+  onError,
+  ...imageProps
+}) => {
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageError = ({ target: { src, alt } }) => {
+    setHasError(true);
+
+    src = fallbackSrc ? fallbackSrc : '';
+    alt = alt ? alt : 'Image failed to load';
+
+    onError && onError(e);
   };
 
-  return <img src={src} alt={alt} className={className} width={width} height={height} onError={handleImageError} />;
+  const handleImageLoad = () => {
+    setIsLoading(false);
+    onLoad && onLoad(e);
+  };
 
+  const displaySrc = isLoading && placeholderSrc ? placeholderSrc : src;
+
+  const loadingAttribute = lazyLoad ? 'lazy' : 'eager';
+
+  return (
+    <div
+      className={`image-wrapper ${wrapperClassName}`}
+      style={{ '--width': width, '--height': height, '--object-fit': objectFit }}
+    >
+      {isLoading && placeholderSrc && (
+        <img
+          className="image"
+          src={placeholderSrc}
+          alt="Image is loading"
+          loading={loadingAttribute}
+        />
+      )}
+      <img
+        src={hasError ? fallbackSrc : displaySrc}
+        alt={alt}
+        className={`image ${imageClassName}`}
+        loading={loadingAttribute}
+        sizes={sizes}
+        onError={(e) => handleImageError(e)}
+        onLoad={handleImageLoad}
+        {...imageProps}
+      />
+    </div>
+  );
 };
 
 export default Image;
